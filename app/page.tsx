a173feb0573f8fc36f63d08d96e551dde566a46d
annatvali <anntvaliashvili@gmail.com>
2024-09-16 02:46:55 +0400

    feat: add custom Button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
+import Button from './components/Button';
+
 const Home: React.FC = () => {
   return (
-    <div className="text-center">
-      <h1>New App</h1>
-    </div>
+    <section className="mt-[77px] flex justify-between">
+      <div></div>
+      <div className="flex gap-[32px]">
+        <Button href={'/add-listing'}>ლისტინგის დამატება</Button>
+        <Button href={'/add-agent'} variant={'secondary'}>
+          აგენტის დამატება
+        </Button>
+      </div>
+    </section>
   );
 };
 
