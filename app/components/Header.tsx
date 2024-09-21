463f25b5039cedc31022329fa7a942c37d0c9ecd
annatvali <anntvaliashvili@gmail.com>
2024-09-22 00:59:56 +0400

    chore: add priority to Logo image in header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,7 +5,13 @@ const Header: React.FC = () => {
   return (
     <header className="border-b border-[primary-gray] px-1 py-[38px] md:px-2 2xl:px-3">
       <Link href={'/'}>
-        <Image src={'/logo.svg'} alt="redberry logo" width={150} height={24} />
+        <Image
+          src={'/logo.svg'}
+          alt="redberry logo"
+          width={150}
+          height={24}
+          priority
+        />
         <h1 className="sr-only">REDBERRY</h1>
       </Link>
     </header>
